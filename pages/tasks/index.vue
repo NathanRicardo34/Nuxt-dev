e1f469efab21a5c3eff84bc45573db634b172aeb
NathanRicardo34 <nathanricardo34@gmail.com>
2023-06-19 15:01:47 -0400

    teste

--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -4,22 +4,25 @@ import { APIParams, FormTask } from '~/models';
 
 const api = new UseAPI()
 const getParams = ref<APIParams>({ endpoint: 'tasks' })
-const form = ref<FormTask>()
+const form = ref<FormTask[]>()
 
 onMounted(async () => await __load())
 
 const __load = async () => {
     try {
-        form.value = await api.get<FormTask>(getParams.value)
+        form.value = await api.get<FormTask[]>(getParams.value)
     } catch(err: any) {
         console.log(err)
     }
 }
 </script>
 <template>
-    <div>
-        <h1>Tasks</h1>
-        {{ form }}
+    <h1>Tasks</h1>
+    <div v-for="(data, idx) in form" :key="idx">
+        <div v-if="data !== undefined && data.description !== undefined && data.completed !== undefined">
+            {{ data.description }}
+            {{ data.completed }}
+        </div>
     </div>
     <span class="text-3xl font-bold underline">
         Hello world!
